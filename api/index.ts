3ed1dc13166f422e6d2836282ccccc68773fd0da
Andy Liu <mingchia.andy.liu@gmail.com>
2022-01-30 13:56:09 -0800

    add confetties

--- api/index.ts
+++ api/index.ts
@@ -15,11 +15,11 @@ export default async function handler(req: IncomingMessage, res: ServerResponse)
             res.end(html);
             return;
         }
-        const { fileType } = parsedReq;
-        const file = await getScreenshot(html, fileType, isDev);
+        const { fileType, confettie } = parsedReq;
+        const file = await getScreenshot(html, fileType, isDev, confettie);
         res.statusCode = 200;
         res.setHeader('Content-Type', `image/${fileType}`);
-        res.setHeader('Cache-Control', `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
+        res.setHeader('Cache-Control', confettie ? 'no-cache, no-store' : `public, immutable, no-transform, s-maxage=31536000, max-age=31536000`);
         res.end(file);
     } catch (e) {
         res.statusCode = 500;
